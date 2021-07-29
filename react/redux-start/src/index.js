2edf52fe983ff1118eb9b5105f452bb50fcb99a5
GoDoyeon <97godo@naver.com>
2021-07-29 17:48:48 +0900

    react-redux 안쓰고 react 연결하기

--- react/redux-start/src/index.js
+++ react/redux-start/src/index.js
@@ -4,22 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
-import { addTodo } from './redux/actions';
-import { completeTodo } from './redux/actions';
-import { showComplete } from './redux/actions';
+import ReduxContext from './contexts/ReduxContext';
 
 // store의 상태가 변경되면 이 함수가 호출됨
-store.subscribe(() => {
-  console.log(store.getState());
-});
+// store.subscribe(() => {
+//   console.log(store.getState());
+// });
 
-store.dispatch(addTodo('할일'));
-store.dispatch(completeTodo(0));
-store.dispatch(showComplete());
+// store.dispatch(addTodo('할일'));
+// store.dispatch(completeTodo(0));
+// store.dispatch(showComplete());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ReduxContext.Provider value={store}>
+      <App />
+    </ReduxContext.Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
